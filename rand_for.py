6d9538562f361382e1313dcda8b6fe72a209aee2
PatrickMal3 <mal3@posteo.de>
2020-09-08 14:41:48 +0200

    rand for

--- rand_for.py
+++ rand_for.py
@@ -9,11 +9,17 @@ from sklearn.metrics import plot_precision_recall_curve
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
 # load in data
 scryData = pd.read_csv('2020_08_22_data_processed/fin_card_data.csv', index_col=0)
 
-y = scryData['exp']
-bulkData = scryData.drop(['exp'], axis=1)
+bulkData = scryData
+#bulkData = scryData[scryData['last_printing'] != 2020]
+#bulkData = bulkData[bulkData['last_printing'] != 2019]
+y = bulkData['exp']
+bulkData = bulkData.drop(['exp'], axis=1)
 bulkData = bulkData.drop(['price'], axis=1)
 #bulkData = bulkData.drop(['edhrec_rank'], axis=1)
 
@@ -21,10 +27,19 @@ bulkData = bulkData.drop(['price'], axis=1)
 #                     'num_printings', 'set_enum', 'is_legendary',
 #                     'edhrec_rank']]
 
+#scaler = StandardScaler()
+#bulkData = scaler.fit_transform(bulkData)
+#bulkData = pd.DataFrame(bulkData, columns=['is_legendary','is_creature','is_instant',
+#    'is_scorcery','is_land','is_enchantment','is_planeswalker','is_artifact','cmc',
+#    'first_printing','last_printing','num_printings','id_white','id_blue','id_black',
+#    'is_red','is_green',
+#    'is_standard_legal','is_commander_legal','is_modern_legal',
+#    'is_pioneer_legal','is_legacy_legal','is_pauper_legal','is_reserved','is_reprint',
+#    'rarity','edhrec_rank','power','toughness','set_enum','is_booster'])
 
 # RF classifier
-RF = RandomForestClassifier(n_estimators=1600, min_samples_split=5, min_samples_leaf=1,
-                            max_features='sqrt', max_depth=70, bootstrap=False)
+RF = RandomForestClassifier(n_estimators=1000, min_samples_split=5, min_samples_leaf=1,
+                            max_features='sqrt', max_depth=30, bootstrap=False)
 
 #RF = RandomForestClassifier()
 # KFold Scoring
@@ -82,11 +97,11 @@ X_train, X_test, y_train, y_test = train_test_split(bulkData, y, test_size=0.33)
 RF.fit(X_train, y_train)
 
 # partial train test split
-newData = scryData[scryData['is_artifact'] == 1]
-y = newData['exp']
-newData = newData.drop(['exp'], axis=1)
-newData = newData.drop(['price'], axis=1)
-X_train, X_test, y_train, y_test = train_test_split(newData, y, test_size=0.33)
+#newData = scryData[scryData['is_artifact'] == 1]
+#y = newData['exp']
+#newData = newData.drop(['exp'], axis=1)
+#newData = newData.drop(['price'], axis=1)
+#X_train, X_test, y_train, y_test = train_test_split(newData, y, test_size=0.33)
 
 pred = RF.predict(X_test)
 print('Train-Test-Split Confusion Matrix:')
@@ -102,10 +117,17 @@ print('Train-Test-Split precision:')
 print(precision_score(pred, y_test))
 print('')
 
-full_pred = RF.predict(newData)
-newData['exp'] = y
-newData['pred'] = full_pred
-newData.to_csv('results.csv', index=True)
+result_df = pd.DataFrame(X_test)
+result_df['exp'] = y_test
+result_df['pred'] = pred
+
+result_df.to_csv('results.csv', index=True)
+
+
+#full_pred = RF.predict(newData)
+#newData['exp'] = y
+#newData['pred'] = full_pred
+#newData.to_csv('results.csv', index=True)
 
 #print('Most important features:')
 #feature_importances = pd.DataFrame(RF.feature_importances_,
